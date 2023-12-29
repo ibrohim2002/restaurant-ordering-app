4b7ea93dfb19a54eeb4b6ccca7db7231cd37fc73
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2023-12-29 22:13:44 +0500

    feat: update files

---
+++ data.js
@@ -0,0 +1,25 @@
+const menuArray = [
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "assets/item1.png",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "assets/item2.png",
+    id: 1,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "assets/item3.png",
+    id: 2,
+  },
+];
+
+export default menuArray;
